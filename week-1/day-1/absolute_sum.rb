36dda6cb9d4555b4fb3b284b25a81870f7448589
Gerard Altamirano <galtamiranor@outlook.es>
2020-04-13 18:45:16 -0500

    Bug fixed

--- week-1/day-1/absolute_sum.rb
+++ week-1/day-1/absolute_sum.rb
@@ -1,28 +1,15 @@
-sum = 0
-
 print "First number: "
 n1 = gets.chomp.to_i
 
 print "Second number: "
 n2 = gets.chomp.to_i
 
-
-=begin 
-Para poder controlar los siguientes casos:
-"n1 negativo o n2 negativo"
-n1 negativo y n2 positivo
-n1 positivo y n2 negativo
-
-n1 negativo y n2 negativo
-=end
-if n1 < 0
-   n1 = n + ((n1) ** 2)
-end 
-
-if n2 < 0
-   n2 = n2 + ((n2) ** 2)
-end
-
+# Entendí mal el assignment lmao, menos mal pude darme cuenta
 sum = n1 + n2
+
+if sum < 0
+#Por identidad multiplicativa
+  sum *= (-1)
+
 
 puts "#{sum}"
